b409f1bab8db8a979d328d0ec4e6641fdc9a15e4
MGitHubb <maria.cioara113@yahoo.com>
2022-04-08 22:30:35 +0300

    added hello world spring boot app

---
+++ src/main/java/com/pad/cuvantar/CuvantarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pad.cuvantar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuvantarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuvantarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pad/cuvantar/CuvantarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pad.cuvantar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuvantarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
