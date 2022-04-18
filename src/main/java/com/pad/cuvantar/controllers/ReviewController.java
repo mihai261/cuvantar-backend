b98002bdea65da296fec935a85b7bfd4c6665eaf
Ioan Hedeș <ihedes13@gmail.com>
2022-04-18 23:17:54 +0300

    added exceptions, changed timestamp logic

--- src/main/java/com/pad/cuvantar/controllers/ReviewController.java
+++ src/main/java/com/pad/cuvantar/controllers/ReviewController.java
@@ -1,8 +1,13 @@
 package com.pad.cuvantar.controllers;
 
+import com.pad.cuvantar.exceptions.InvalidTokenException;
+import com.pad.cuvantar.exceptions.ReviewAlreadyExistsException;
+import com.pad.cuvantar.exceptions.ReviewNotFoundException;
+import com.pad.cuvantar.exceptions.UserNotFoundException;
 import com.pad.cuvantar.models.ReviewModel;
 import com.pad.cuvantar.services.AuthService;
 import com.pad.cuvantar.services.ReviewService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,20 +24,22 @@ public class ReviewController {
     @Resource
     ReviewService reviewService;
 
-    // doesn't work yet, java timezone different from sql timezone :(
     @GetMapping("/find")
-    public List<ReviewModel> getReviewsForUser(@RequestParam String username) {
+    public List<ReviewModel> getReviewsForUser(@RequestParam String username, @RequestHeader("custom-token") String token) throws UserNotFoundException, InvalidTokenException {
 
-        if (!authService.checkAuthSessionExists(username)) throw new RuntimeException();
+        if (!authService.checkAuthToken(username, token)) throw new InvalidTokenException("Provided token is invalid");
 
         return reviewService.getReviewsForUser(username);
     }
 
     @PatchMapping("/modify")
-    public void updateReview(@RequestParam String username, @RequestParam int cardId,
-                                    @RequestParam Optional<Boolean> passed) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateReview(@RequestParam String username,
+                             @RequestParam int cardId,
+                             @RequestParam Optional<Boolean> passed,
+                             @RequestHeader("custom-token") String token) throws UserNotFoundException, InvalidTokenException, ReviewNotFoundException, ReviewAlreadyExistsException {
 
-        if (!authService.checkAuthSessionExists(username)) throw new RuntimeException();
+        if (!authService.checkAuthToken(username, token)) throw new InvalidTokenException("Provided token is invalid");
 
         if (passed.isPresent()) {
             reviewService.updateReviewForUser(username, cardId, passed.get());
--- src/main/java/com/pad/cuvantar/handlers/RestExceptionHandler.java
+++ src/main/java/com/pad/cuvantar/handlers/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pad.cuvantar.handlers;
 
 import com.pad.cuvantar.exceptions.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
     @ResponseBody
-    @ExceptionHandler(value = {UserNotFoundException.class})
+    @ExceptionHandler(value = {UserNotFoundException.class, ReviewNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String notFoundExceptionHandler(Exception e){
         return e.getMessage();
     }
 
     @ResponseBody
-    @ExceptionHandler(value = {SessionException.class})
+    @ExceptionHandler(value = {SessionException.class, MissingRequestHeaderException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     String badRequestExceptionHandler(Exception e){
         return e.getMessage();
@@ -31,7 +32,7 @@ public class RestExceptionHandler {
     }
 
     @ResponseBody
-    @ExceptionHandler(value = {UserAlreadyExistsException.class, EmailAlreadyExistsException.class})
+    @ExceptionHandler(value = {UserAlreadyExistsException.class, EmailAlreadyExistsException.class, ReviewAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
     String conflictHandler(Exception e){
         return e.getMessage();
