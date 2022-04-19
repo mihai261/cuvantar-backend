2073824e2da911bfb4a9fb657d17e10d0c10f059
Mihai Parvulescu <mihai1309f@gmail.com>
2022-04-19 10:51:57 +0300

    small tweak to swagger ui

--- src/main/java/com/pad/cuvantar/CuvantarBackendApplication.java
+++ src/main/java/com/pad/cuvantar/CuvantarBackendApplication.java
@@ -1,9 +1,12 @@
 package com.pad.cuvantar;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Cuvantar", description = "Some long and useful description which I didn't bother writing...", version = "v1"))
 public class CuvantarBackendApplication {
 
 	public static void main(String[] args) {
